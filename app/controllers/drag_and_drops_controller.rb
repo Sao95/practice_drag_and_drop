1c17a0662120b3d16ba725ebc44a6a086b25b1cb
Sao95 <merasaori@gmail.com>
2022-02-17 13:12:48 +0000

    [Add] ドラッグ、ドロップ

---
+++ app/controllers/drag_and_drops_controller.rb
@@ -0,0 +1,4 @@
+class DragAndDropsController < ApplicationController
+  def index
+  end
+end
---
+++ test/controllers/drag_and_drops_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DragAndDropsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
